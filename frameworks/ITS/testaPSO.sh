0e928648aa9636d455aa542540dcc78e267f1f71
Tiago Trotta <mrtrotta2010@gmail.com>
2019-05-31 15:18:20 -0300

    Testes rodados para o PSO

--- frameworks/ITS/testaPSO.sh
+++ frameworks/ITS/testaPSO.sh
@@ -4,6 +4,7 @@
 #$2 é o recomendador
 
 #teste=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29)
+#nuvem=(60 65 70 75 80 85 90 95 100)
 nuvem=(1 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100)
 #teste=(0 1)
 
@@ -15,5 +16,5 @@ do
    	# Gera um arquivo de teste aleatório
    	#./randomTestITS ../../Evaluations/ITS_Output/ML-1M/alfa0.5/userKNN$t/
    	# Roda o ITS com o arquivo de testes gerado
-   	./its PSO ../../Datasets/ML-1M/ ../../Recommendations-Lists/ML-1M/rec_userKNN_conv.txt ../../Evaluations/MDRS_Output/ML-1M/alfa0.5/userKNN$v 100 0.5 10 $n
+   	./its PSO ../../Datasets/ML-1M/ ../../Recommendations-Lists/ML-1M/rec_userKNN_conv.txt ../../Evaluations/MDRS_Output/ML-1M/alfa0.5/userKNN$n 100 0.5 10 $n
 done
